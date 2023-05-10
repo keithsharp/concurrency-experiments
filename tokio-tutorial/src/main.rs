a52a30c759f3fb72617def181a0d0d3c8444e99b
Keith Sharp <kms@passback.co.uk>
2023-05-10 12:26:37 +0100

    WIP: tokio-tutorial Shared State complete.

--- tokio-tutorial/src/main.rs
+++ tokio-tutorial/src/main.rs
@@ -1,26 +1,31 @@
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
+use bytes::Bytes;
 use mini_redis::Command::{self, Get, Set};
 use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let db = Arc::new(Mutex::new(HashMap::new()));
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
     loop {
         let (socket, _) = listener.accept().await?;
+        let db = db.clone();
+        println!("Accepted socket, processing");
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 
     // Ok(())
 }
 
-async fn process(socket: TcpStream) {
-    let mut db = HashMap::new();
-
+async fn process(socket: TcpStream, db: Db) {
     let mut connection = Connection::new(socket);
     while let Some(frame) = connection
         .read_frame()
@@ -31,10 +36,12 @@ async fn process(socket: TcpStream) {
             .expect("should always be able to get a command from a frame")
         {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().expect("should be able to acquire lock on db");
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().expect("should be able to acquire lock on db");
                 if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
