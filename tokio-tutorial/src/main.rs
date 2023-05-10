d813a5a46a505c9d1ea4bc0892af7d7aa13be49d
Keith Sharp <kms@passback.co.uk>
2023-05-10 11:54:16 +0100

    WIP: tokio-tutorial first part of spawning.

--- tokio-tutorial/src/main.rs
+++ tokio-tutorial/src/main.rs
@@ -1,13 +1,34 @@
-use mini_redis::{client, Result};
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    client.set("hello", "world".into()).await?;
+    loop {
+        let (socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
 
-    let res = client.get("hello").await?;
-    println!("Response from server: {:?}", res);
+    // Ok(())
+}
 
-    Ok(())
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
+
+    match connection.read_frame().await {
+        Ok(frame) => {
+            println!("FRAME: {:?}", frame);
+            match connection
+                .write_frame(&Frame::Error("Unimplemented".to_string()))
+                .await
+            {
+                Ok(_) => (),
+                Err(e) => eprintln!("{}", e),
+            }
+        }
+        Err(e) => eprintln!("{}", e),
+    }
 }
