1e3cab4f49120a2a2cdfe6719a01154d9706cc7b
Keith Sharp <kms@passback.co.uk>
2023-05-14 17:11:31 +0100

    Passing messages between Tokio tasks with mpsc and oneshot channels.

--- mpsc-oneshot/src/main.rs
+++ mpsc-oneshot/src/main.rs
@@ -2,12 +2,18 @@ use std::thread::sleep;
 use std::time::Duration;
 
 use tokio::sync::mpsc;
+use tokio::sync::mpsc::error::TryRecvError;
 use tokio::sync::oneshot;
 
-use tokio::sync::mpsc::error::TryRecvError;
 use tokio::task;
 
 use uuid::Uuid;
+
+#[derive(Debug)]
+enum Event {
+    Work(Work),
+    Quit,
+}
 
 #[derive(Debug)]
 struct Work {
@@ -16,12 +22,14 @@ struct Work {
 }
 
 #[derive(Debug)]
-struct Response(Uuid);
+enum Response {
+    Complete(Uuid),
+    Quitting,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let (m_tx, m_rx) = mpsc::channel::<(Work, oneshot::Sender<Response>)>(100);
-    let (o_tx, o_rx) = oneshot::channel();
+    let (m_tx, m_rx) = mpsc::channel::<(Event, oneshot::Sender<Response>)>(100);
 
     let handle = task::spawn_blocking(move || {
         perform_work(m_rx);
@@ -33,27 +41,48 @@ async fn main() -> anyhow::Result<()> {
     };
 
     println!("Sending work.");
-    m_tx.send((work, o_tx)).await?;
+    let (o_tx, o_rx) = oneshot::channel();
+    m_tx.send((Event::Work(work), o_tx)).await?;
 
     let response = o_rx.await?;
-    println!("Got response ID: {}", response.0);
+    match response {
+        Response::Complete(id) => println!("Got response ID: {}", id),
+        _ => (),
+    }
+
+    println!("Sending quit.");
+    let (o_tx, o_rx) = oneshot::channel();
+    m_tx.send((Event::Quit, o_tx)).await?;
+
+    let response = o_rx.await?;
+    match response {
+        Response::Quitting => println!("Work loop is quitting."),
+        _ => (),
+    }
 
     handle.await?;
 
-    println!("Shutting down.");
+    println!("Exiting program.");
     Ok(())
 }
 
-fn perform_work(mut rx: mpsc::Receiver<(Work, oneshot::Sender<Response>)>) {
+fn perform_work(mut rx: mpsc::Receiver<(Event, oneshot::Sender<Response>)>) {
     loop {
         match rx.try_recv() {
-            Ok((work, o_tx)) => {
-                println!("Performing '{}' for {} seconds", work.id, work.amount);
-                sleep(Duration::from_secs(work.amount));
-                o_tx.send(Response(work.id)).unwrap();
-            }
+            Ok((event, o_tx)) => match event {
+                Event::Work(work) => {
+                    println!("Performing '{}' for {} seconds", work.id, work.amount);
+                    sleep(Duration::from_secs(work.amount));
+                    o_tx.send(Response::Complete(work.id)).unwrap();
+                }
+                Event::Quit => {
+                    println!("Perform work got quit message.");
+                    o_tx.send(Response::Quitting).unwrap();
+                    break;
+                }
+            },
             Err(TryRecvError::Empty) => {
-                // println!("No work to do, sleeping for 0.5 seconds");
+                println!("No work to do, sleeping for 0.5 seconds");
                 sleep(Duration::from_millis(500));
             }
             Err(TryRecvError::Disconnected) => {
